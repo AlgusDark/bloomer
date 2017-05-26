f10c46dec24a8d0290c4fc6b25d428a647971114
Carlos Perez <cperez@eharmony.com>
2017-05-25 20:33:47 -0500

    Add Bulma and Grid tests for get*Modifiers

--- test/bulma.test.ts
+++ test/bulma.test.ts
@@ -62,6 +62,87 @@ describe('removeProps functions', () => {
     });
 });
 
-describe.skip('get*Modifiers functions', () => {
-    
+describe('get*Modifiers functions', () => {
+    it('should getAlignmentModifiers', () => {
+        const props = {
+            isCentered: true,
+            isRight: true,
+        }
+        const expected = {
+            'is-centered': true,
+            'is-right': true,
+        }
+        expect(Bulma.getAlignmentModifiers(props))
+            .toEqual(expected);
+    });
+
+    it('should getSizeModifiers', () => {
+        const props = {
+            isSmall: true,
+            isMedium: true,
+            isLarge: true,
+        }
+        const expected = {
+            'is-small': true,
+            'is-medium': true,
+            'is-large': true,
+        }
+        expect(Bulma.getSizeModifiers(props))
+            .toEqual(expected);
+    });
+
+    it('should getFullWidthModifiers', () => {
+        const props = {
+            isFullWidth: true,
+        }
+        const expected = {
+            'is-fullwidth': true,
+        }
+        expect(Bulma.getFullWidthModifiers(props))
+            .toEqual(expected);
+    });
+
+    it('should getStateModifiers', () => {
+        const props = {
+            isActive: true,
+            isFocused: true,
+            isHovered: true,
+            isLoading: true,
+        }
+        const expected = {
+            'is-active': true,
+            'is-focused': true,
+            'is-hovered': true,
+            'is-loading': true,
+        }
+        expect(Bulma.getStateModifiers(props))
+            .toEqual(expected);
+    });
+
+    it('should getColorModifiers', () => {
+        const props = {
+            isWhite: true,
+            isLight: true,
+            isDark: true,
+            isBlack: true,
+            isPrimary: true,
+            isInfo: true,
+            isSuccess: true,
+            isWarning: true,
+            isDanger: true,
+        }
+        const expected = {
+            'is-white': true,
+            'is-light': true,
+            'is-dark': true,
+            'is-black': true,
+            'is-primary': true,
+            'is-info': true,
+            'is-success': true,
+            'is-warning': true,
+            'is-danger': true,
+        }
+        expect(Bulma.getColorModifiers(props))
+            .toEqual(expected);
+    });
 })
