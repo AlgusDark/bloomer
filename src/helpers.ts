407ee27215cdd04dd19b88bd7c9070d3f88148db
Carlos Perez <cperez@eharmony.com>
2017-06-05 12:22:55 -0500

    Fix interface Grid

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,11 @@
 import { Bulma } from './bulma';
 
-export function getHTMLProps(props: Bulma.NonHTMLProps, ...args: Array<Function>): React.HTMLProps<HTMLElement> {
+export function getHTMLProps(props: Bulma.NonHTMLProps | Bulma.Grid, ...args: Array<Function>): React.HTMLProps<HTMLElement> {
     return args.length > 0 ? args.reduce((rest, fn) => ({ ...fn(rest) }), props) : props
 }
 
-export function combineModifiers(props: Bulma.NonHTMLProps, ...args: Array<Function>): object {
-    return args.length > 0 ? args.reduce((rest, fn) => ({...rest, ...fn(props) }), {}) : {};
+export function combineModifiers(props: Bulma.NonHTMLProps | Bulma.Grid, ...args: Array<Function>): object {
+    return args.length > 0 ? args.reduce((rest, fn) => ({ ...rest, ...fn(props) }), {}) : {};
 }
 
 export const isBetween = (min: number, max: number) => (value: number) => (value >= min && value <= max);
