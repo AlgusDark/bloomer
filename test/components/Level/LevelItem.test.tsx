e5a7701fcb78d4068a1d99b9fbd2c5ef1320fc4d
Algus Dark <algus.dark@gmail.com>
2017-06-10 14:49:40 -0500

    LevelItem now can be a div (default) or an anchor with href prop.

--- test/components/Level/LevelItem.test.tsx
+++ test/components/Level/LevelItem.test.tsx
@@ -9,6 +9,11 @@ describe('LevelItem', () => {
         expect(component.contains(<div className='level-item'>My LevelItem</div>)).toBe(true);
     });
 
+    it('should render an anchor with .level-item', () => {
+        const component = shallow(<LevelItem href='#'>My LevelItem</LevelItem>);
+        expect(component.contains(<a href='#' className='level-item'>My LevelItem</a>)).toBe(true);
+    });
+
     it('should render a div with .level-item and modifiers', () => {
         const component = shallow(<LevelItem isFlexible>My LevelItem</LevelItem>);
         expect(component.hasClass('level-item')).toBe(true);
