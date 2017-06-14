de61a283114787f1be39fba9ef28128a306be85c
Algus Dark <algus.dark@gmail.com>
2017-06-14 18:59:48 -0500

    Update entry point as a Single Source of Truth

--- src/index.ts
+++ src/index.ts
@@ -1,131 +1,122 @@
-export {
-    Card,
-    Image as CardImage,
-    Header as CardHeader,
-    Title as CardHeaderTitle,
-    Icon as CardHeaderIcon,
-    Content as CardContent,
-    Footer as CardFooter,
-    Item as CardFooterItem,
-} from './components/Card';
+// Grid
+export { default as Columns } from './grid/Columns';
+export { default as Column } from './grid/Column';
+export { default as Tile } from './grid/Tile';
 
-export {
-    Level,
-    Item as LevelItem,
-    Left as LevelLeft,
-    Right as LevelRight,
-} from './components/Level';
+// Elements
+export { default as Box } from './elements/Box';
+export { default as Button } from './elements/Button';
+export { default as Content } from './elements/Content';
+export { default as Delete } from './elements/Delete';
+export { default as Icon } from './elements/Icon';
+export { default as Image } from './elements/Image';
+export { default as Notification } from './elements/Notification';
+export { default as Progress } from './elements/Progress';
+export { default as Table } from './elements/Table';
+export { default as Tag } from './elements/Tag';
+export { default as Title } from './elements/Title';
+export { default as Subtitle } from './elements/Subtitle';
+export { default as Heading } from './elements/Heading';
 
-export {
-    Media,
-    Content as MediaContent,
-    Left as MediaLeft,
-    Right as MediaRight,
-} from './components/Media';
+// Form
+export { default as Checkbox } from './elements/Form/Checkbox';
+export { default as Control } from './elements/Form/Control';
+export { default as Help } from './elements/Form/Help';
+export { default as Input } from './elements/Form/Input';
+export { default as Label } from './elements/Form/Label';
+export { default as Radio } from './elements/Form/Radio';
+export { default as Select } from './elements/Form/Select';
+export { default as TextArea } from './elements/Form/TextArea';
+// Form - Field
+export { default as Field } from './elements/Form/Field/Field';
+export { default as FieldBody } from './elements/Form/Field/FieldBody';
+export { default as FieldLabel } from './elements/Form/Field/FieldLabel';
 
-export {
-    Menu,
-    Label as MenuLabel,
-    List as MenuList,
-    Link as MenuLink,
-} from './components/Menu';
+// Card
+export { default as Card } from './components/Card/Card';
+export { default as CardImage } from './components/Card/CardImage';
+export { default as CardContent } from './components/Card/CardContent';
+// Card - Header
+export { default as CardHeader } from './components/Card/Header/CardHeader';
+export { default as CardHeaderTitle } from './components/Card/Header/CardHeaderTitle';
+export { default as CardHeaderIcon } from './components/Card/Header/CardHeaderIcon';
+// Card - Footer
+export { default as CardFooter } from './components/Card/Footer/CardFooter';
+export { default as CardFooterItem } from './components/Card/Footer/CardFooterItem';
 
-export {
-    Message,
-    Header as MessageHeader,
-    Body as MessageBody,
-} from './components/Message';
+// Level
+export { default as Level } from './components/Level/Level';
+export { default as LevelItem } from './components/Level/LevelItem';
+export { default as LevelLeft } from './components/Level/LevelLeft';
+export { default as LevelRight } from './components/Level/LevelRight';
 
-export {
-    Modal,
-    ModalBackground,
-    ModalContent,
-    ModalClose,
+// Media
+export { default as Media } from './components/Media/Media';
+export { default as MediaContent } from './components/Media/MediaContent';
+export { default as MediaLeft } from './components/Media/MediaLeft';
+export { default as MediaRight } from './components/Media/MediaRight';
 
-    ModalCard,
-    ModalCardHeader,
-    ModalCardTitle,
-    ModalCardBody,
-    ModalCardFooter,
-} from './components/Modal';
+// Menu
+export { default as Menu } from './components/Menu/Menu';
+export { default as MenuLabel } from './components/Menu/MenuLabel';
+export { default as MenuList } from './components/Menu/MenuList';
+export { default as MenuLink } from './components/Menu/MenuLink';
 
-export {
-    Nav,
-    Left as NavLeft,
-    Center as NavCenter,
-    Right as NavRight,
-    Toggle as NavToggle,
-    Item as NavItem,
-} from './components/Nav';
+// Message
+export { default as Message } from './components/Message/Message';
+export { default as MessageHeader } from './components/Message/MessageHeader';
+export { default as MessageBody } from './components/Message/MessageBody';
 
-export {
-    Pagination,
-    Control as PageControl,
-    Ellipsis as PageEllipsis,
-    Page,
-    List as PageList,
-    Link as PageLink,
-} from './components/Pagination';
+// Modal
+export { default as Modal } from './components/Modal/Modal';
+export { default as ModalBackground } from './components/Modal/ModalBackground';
+export { default as ModalContent } from './components/Modal/ModalContent';
+export { default as ModalClose } from './components/Modal/ModalClose';
+// ModalCard
+export { default as ModalCard } from './components/Modal/Card/ModalCard';
+export { default as ModalCardHeader } from './components/Modal/Card/ModalCardHeader';
+export { default as ModalCardTitle } from './components/Modal/Card/ModalCardTitle';
+export { default as ModalCardBody } from './components/Modal/Card/ModalCardBody';
+export { default as ModalCardFooter } from './components/Modal/Card/ModalCardFooter';
 
-export {
-    Panel,
-    Heading as PanelHeading,
-    Tabs as PanelTabs,
-    Tab as PanelTab,
-    Block as PanelBlock,
-    Icon as PanelIcon,
-} from './components/Panel';
+// Nav
+export { default as Nav } from './components/Nav/Nav';
+export { default as NavLeft } from './components/Nav/NavLeft';
+export { default as NavCenter } from './components/Nav/NavCenter';
+export { default as NavRight } from './components/Nav/NavRight';
+export { default as NavToggle } from './components/Nav/NavToggle';
+export { default as NavItem } from './components/Nav/NavItem';
 
-export {
-    Tabs,
-    Tab,
-    List as TabList,
-    Link as TabLink,
-} from './components/Tabs';
+// Pagination
+export { default as Pagination } from './components/Pagination/Pagination';
+export { default as PageControl } from './components/Pagination/PageControl';
+export { default as PageEllipsis } from './components/Pagination/Ellipsis';
+export { default as Page } from './components/Pagination/Page';
+export { default as PageList } from './components/Pagination/PageList';
+export { default as PageLink } from './components/Pagination/PageLink';
 
-export {
-    Box,
-    Button,
-    Content,
-    Delete,
-    Icon,
-    Image,
-    Notification,
-    Progress,
-    Table,
-    Tag,
-    Title,
-    Subtitle,
-    Heading,
-} from './elements';
+// Panel
+export { default as Panel } from './components/Panel/Panel';
+export { default as PanelHeading } from './components/Panel/PanelHeading';
+export { default as PanelTabs } from './components/Panel/PanelTabs';
+export { default as PanelTab } from './components/Panel/PanelTab';
+export { default as PanelBlock } from './components/Panel/PanelBlock';
+export { default as PanelIcon } from './components/Panel/PanelIcon';
 
-export {
-    Field, FieldBody, FieldLabel,
-    Checkbox,
-    Control,
-    Help,
-    Input,
-    Label,
-    Radio,
-    Select,
-    TextArea,
-} from './elements/Form/';
+// Tabs
+export { default as Tabs } from './components/Tabs/Tabs';
+export { default as Tab } from './components/Tabs/Tab';
+export { default as TabList } from './components/Tabs/TabList';
+export { default as TabLink } from './components/Tabs/TabLink';
 
-export {
-    Columns,
-    Column,
-    Tile,
-} from './grid';
-
-export {
-    Container,
-    Footer,
-    Section,
-    Hero,
-    HeroHeader,
-    HeroBody,
-    HeroVideo,
-    HeroFooter,
-} from './layout'
+// Layout
+export { default as Container } from './layout/Container';
+export { default as Footer } from './layout/Footer';
+export { default as Section } from './layout/Section';
+export { default as Hero } from './layout/Hero/Hero';
+export { default as HeroHeader } from './layout/Hero/HeroHeader';
+export { default as HeroBody } from './layout/Hero/HeroBody';
+export { default as HeroVideo } from './layout/Hero/HeroVideo';
+export { default as HeroFooter } from './layout/Hero/HeroFooter';
 
 export { withHelpersModifiers } from './bulma';
