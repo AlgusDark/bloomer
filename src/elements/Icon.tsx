6cce90139b57d435746683f5599cbb5a416d3349
Algus Dark <algus.dark@gmail.com>
2017-06-14 20:03:49 -0500

    Refactor Icon Element

--- src/elements/Icon.tsx
+++ src/elements/Icon.tsx
@@ -12,6 +12,7 @@ import { getHTMLProps, isOption } from './../helpers';
 export interface Icon<T> extends Bulma.Size, Bulma.Alignment,
     React.HTMLProps<T> {
     isAlign?: 'left' | 'right';
+    icon?: string;
 }
 
 export function Icon(props: Icon<HTMLSpanElement>) {
@@ -19,17 +20,22 @@ export function Icon(props: Icon<HTMLSpanElement>) {
     const className = classNames(
         'icon',
         {
-            ...(isAlignOption(props.isAlign) ? {[`is-${props.isAlign}`]: true} : {}),
+            ...(isAlignOption(props.isAlign) ? { [`is-${props.isAlign}`]: true } : {}),
             ...getSizeModifiers(props),
         },
         props.className,
     );
 
-    const HTMLProps = getHTMLProps(props, removeAlignmentProps, removeSizeProps);
+    const { icon, children, ...rest } = props;
+    const HTMLProps = getHTMLProps(rest, removeAlignmentProps, removeSizeProps);
 
-    return (
-        <span {...HTMLProps} className={className} />
+    const withIcon = (
+        <span {...HTMLProps} className={className}>
+            <span className={`fa fa-${icon}`} aria-hidden="true"></span>
+        </span>
     )
+
+    return icon ? withIcon : <span {...HTMLProps} children={children} className={className} />
 }
 
 export default withHelpersModifiers(Icon);
