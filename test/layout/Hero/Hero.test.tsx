4d1b0fd3b38adee59a00be642fcec623ce25b972
Algus Dark <algus.dark@gmail.com>
2017-06-16 21:32:20 -0500

    Add more tests for Layout

--- test/layout/Hero/Hero.test.tsx
+++ test/layout/Hero/Hero.test.tsx
@@ -16,6 +16,22 @@ describe('Hero', () => {
         expect(container.hasClass('is-fullheight')).toBe(true);
     });
 
+    it('should render a section with .hero and color modifiers', () => {
+        const container = shallow(<Hero isBold isFullHeight isColor='light' />);
+        expect(container.hasClass('hero')).toBe(true);
+        expect(container.hasClass('is-bold')).toBe(true);
+        expect(container.hasClass('is-fullheight')).toBe(true);
+        expect(container.hasClass('is-light')).toBe(true);
+    });
+
+    it('should render a section with .hero and size modifiers', () => {
+        const container = shallow(<Hero isBold isFullHeight isSize='medium' />);
+        expect(container.hasClass('hero')).toBe(true);
+        expect(container.hasClass('is-bold')).toBe(true);
+        expect(container.hasClass('is-fullheight')).toBe(true);
+        expect(container.hasClass('is-medium')).toBe(true);
+    });
+
     it('should render a section with .hero, modifiers and custom classNames', () => {
         const container = shallow(<Hero isBold className='custom loader' />);
         expect(container.hasClass('hero')).toBe(true);
