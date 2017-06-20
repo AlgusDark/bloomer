ae1e74383345de405a9cc4ec25edc316978303dc
Algus Dark <algus.dark@gmail.com>
2017-06-20 12:59:30 -0500

    Remove tag prop to Icon since is having some issues with TS

--- src/components/Tabs/TabLink.tsx
+++ src/components/Tabs/TabLink.tsx
@@ -7,7 +7,7 @@ export interface TabLink<T> extends Bulma.Render, Bulma.Tag,  React.HTMLProps<T>
 export function TabLink({ tag = 'a', render, ...props }: TabLink<HTMLElement>) {
     if (render) return render({ ...props });
 
-    return React.createElement(tag, { ...props });
+    return React.createElement(tag, props);
 }
 
 const HOC = /*@__PURE__*/withHelpersModifiers(TabLink);
--- test/components/Panel/PanelIcon.test.tsx
+++ test/components/Panel/PanelIcon.test.tsx
@@ -9,18 +9,14 @@ describe('PanelIcon', () => {
         expect(component.contains(<span className='panel-icon'>My PanelIcon</span>)).toBe(true);
     });
 
-    it('should render a span with .panel-icon', () => {
-        const component = shallow(<PanelIcon tag='i' icon='home' />);
+    it('should render a span.icon with span.fa.fa-home', () => {
+        const component = shallow(<PanelIcon icon='home' />);
         expect(component.contains(
             <span className='panel-icon'>
-                <i className='fa fa-home' aria-hidden="true" />
+                <span className='fa fa-home' aria-hidden="true">
+                </span>
             </span>
         )).toBe(true);
-    });
-
-    it('should render a p with .panel-icon', () => {
-        const component = shallow(<PanelIcon tag='p'>My PanelIcon</PanelIcon>);
-        expect(component.contains(<p className='panel-icon'>My PanelIcon</p>)).toBe(true);
     });
 
     it('should render a span with .panel-icon and custom classNames', () => {
