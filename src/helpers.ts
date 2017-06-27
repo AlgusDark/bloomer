7d04d00dd69dcf5e7e6a3c2c3b21bee2420b1b81
omganunicorn <kralos@gmail.com>
2017-06-27 17:23:18 -0500

    Updates the array rule in tslint.json and the corresponding errors in the code.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,13 @@
 import { Bulma } from './bulma';
 
-export function getHTMLProps(props: object, ...args: Array<Function>): React.HTMLProps<HTMLElement> {
+export function getHTMLProps(props: object, ...args: Function[]): React.HTMLProps<HTMLElement> {
     return args.length > 0 ? args.reduce((rest, fn) => ({ ...fn(rest) }), props) : props;
 }
 
-export function combineModifiers(props: object, ...args: Array<Function>): object {
+export function combineModifiers(props: object, ...args: Function[]): object {
     return args.length > 0 ? args.reduce((rest, fn) => ({ ...rest, ...fn(props) }), {}) : {};
 }
 
 export const isBetween = (min: number, max: number) => (value: number) => (value >= min && value <= max);
 export const is = (options: object) => (str: string): boolean => options[str] || false;
-export const isOption = (...fn: Array<Function>) => (str: string | boolean) => fn.some((option) => option(str));
+export const isOption = (...fn: Function[]) => (str: string | boolean) => fn.some((option) => option(str));
