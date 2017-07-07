92bbe4ddf5855cd64e58b8846793fc8506e850bd
Algus Dark <algus.dark@gmail.com>
2017-07-07 01:56:54 -0500

    Change Navigation docs menu to use the new Navbar component instead of Nav.

--- docs/src/Components/NavBar.tsx
+++ docs/src/Components/NavBar.tsx
@@ -1,59 +1,118 @@
 import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom'
 
 import { Container, Control, Icon, Field, Button, Image } from './../../../src';
-import { Nav, NavLeft as Left, NavCenter as Center, NavToggle as Toggle, NavRight as Right, NavItem as Item } from './../../../src'
+import { Navbar, NavbarBrand, NavbarBurger, NavbarDivider, NavbarDropdown, NavbarEnd, NavbarStart, NavbarItem, NavbarLink, NavbarMenu } from './../../../src'
+import { isActive } from './../utils';
 
 const brand = require('./../../assets/logo.png');
+const pkg = require('./../../../package.json');
 
-class NavBar extends React.PureComponent<{}, { isActive: boolean }> {
-    state = { isActive: false };
+interface Navbar extends RouteComponentProps<{}> {
+
+}
+
+const NavbarItems = ({ location }: Navbar) => {
+    const pages = [
+        {
+            href: '/documentation/overview',
+            title: 'Overview'
+        },
+        {
+            href: '/documentation/grid',
+            title: 'Grid'
+        },
+        {
+            href: '/documentation/elements',
+            title: 'Elements'
+        },
+        {
+            href: '/documentation/components',
+            title: 'Components'
+        },
+        {
+            href: '/documentation/layout',
+            title: 'Layout'
+        },
+    ]
+
+    return (
+        <div>
+            {pages.map((page, i) => (
+                <NavbarItem isActive={isActive(location, page.href)} key={i} href={'#' + page.href}>{page.title}</NavbarItem>
+            ))}
+        </div>
+    )
+}
+
+class NavBar extends React.Component<Navbar, { isActive: boolean, isDropdownOpen: boolean }> {
+    state = { isActive: false, isDropdownOpen: false };
 
     onClickNav = () => {
         this.setState(state => ({ isActive: !state.isActive }));
     }
 
+    onClickDropdown = () => {
+        this.setState(state => ({ isDropdownOpen: !state.isDropdownOpen }));
+    }
+
     render() {
         return (
-            <Nav>
-                <Container>
-                    <Left>
-                        <Item isBrand href='http://bloomer.js.org'>
-                            <img src={brand} style={{marginRight: 5}} /> Bloomer
-                        </Item>
-                    </Left>
-                    <Center>
-                        <Item href="https://github.com/AlgusDark/bloomer">
-                            <Icon>
-                                <span className="fa fa-github" aria-hidden="true" />
-                            </Icon>
-                        </Item>
-                        <Item href="https://twitter.com/AlgusDark">
-                            <Icon>
-                                <span className="fa fa-twitter" aria-hidden="true" />
-                            </Icon>
-                        </Item>
-                    </Center>
-                    <Toggle isActive={this.state.isActive} onClick={this.onClickNav} />
-                    <Right isMenu isActive={this.state.isActive} onClick={this.onClickNav}>
-                        <Item href="#/">Home</Item>
-                        <Item href="#/documentation">Documentation</Item>
-                        <Item>
-                            <Field isGrouped>
-                                <Control>
-                                    <Button id="twitter" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://bloomer.js.org" target="_blank" href="https://twitter.com/intent/tweet?text=bloomer: a set of React Stateless Components for bulma.io&amp;url=http://bloomer.js.org&amp;via=AlgusDark">
-                                        <Icon>
-                                            <span className="fa fa-twitter" aria-hidden="true" />
-                                        </Icon>
-                                        <span>Tweet</span>
-                                    </Button>
-                                </Control>
-                            </Field>
-                        </Item>
-                    </Right>
-                </Container>
-            </Nav>
+            <Container>
+                <Navbar>
+                    <NavbarBrand>
+                        <NavbarItem href='http://bloomer.js.org'>
+                            <img src={brand} style={{ marginRight: 5 }} /> Bloomer
+                        </NavbarItem>
+                        <NavbarItem href="https://github.com/AlgusDark/bloomer" isHidden='desktop'>
+                            <Icon icon='github' />
+                        </NavbarItem>
+                        <NavbarItem href="https://twitter.com/AlgusDark" isHidden='desktop'>
+                            <Icon icon='twitter' style={{ color: '#55acee' }} />
+                        </NavbarItem>
+                        <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+                    </NavbarBrand>
+                    <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+                        <NavbarStart>
+                            <NavbarItem href='#/'>Home</NavbarItem>
+                            <NavbarItem onClick={this.onClickDropdown} hasDropdown isActive={this.state.isDropdownOpen}>
+                                <NavbarLink isActive={isActive(this.props.location, 'documentation')}>Documentation</NavbarLink>
+                                <NavbarDropdown>
+                                    <NavbarItems {...this.props} />
+                                    <NavbarDivider />
+                                    <NavbarItem>
+                                        <div>
+                                            version <p className='has-text-info'>{pkg.version}</p>
+                                        </div>
+                                    </NavbarItem>
+                                </NavbarDropdown>
+                            </NavbarItem>
+                        </NavbarStart>
+                        <NavbarEnd>
+                            <NavbarItem href="https://github.com/AlgusDark/bloomer" isHidden='touch'>
+                                <Icon icon='github' />
+                            </NavbarItem>
+                            <NavbarItem href="https://twitter.com/AlgusDark" isHidden='touch'>
+                                <Icon icon='twitter' style={{ color: '#55acee' }} />
+                            </NavbarItem>
+                            <NavbarItem>
+                                <Field isGrouped>
+                                    <Control>
+                                        <Button id="twitter" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://bloomer.js.org" target="_blank" href="https://twitter.com/intent/tweet?text=bloomer: a set of React Stateless Components for bulma.io&amp;url=http://bloomer.js.org&amp;via=AlgusDark">
+                                            <Icon>
+                                                <span className="fa fa-twitter" aria-hidden="true" />
+                                            </Icon>
+                                            <span>Tweet</span>
+                                        </Button>
+                                    </Control>
+                                </Field>
+                            </NavbarItem>
+                        </NavbarEnd>
+                    </NavbarMenu>
+                </Navbar>
+            </Container>
         )
     }
 }
 
-export default NavBar;
+export default withRouter<{}>(NavBar);
