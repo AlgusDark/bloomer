9aa1686dd4766d414dce8d65eedb042ba2536c05
Algus Dark <algus.dark@gmail.com>
2017-07-13 19:11:54 -0500

    Fix Test

--- test/components/Navbar/NavbarMenu.test.tsx
+++ test/components/Navbar/NavbarMenu.test.tsx
@@ -4,30 +4,30 @@ import { shallow } from 'enzyme';
 import { NavbarMenu } from './../../../src/components/Navbar/NavbarMenu';
 
 describe('NavbarMenu', () => {
-    it('should render a nav with .navbar-menu', () => {
-        const component = shallow(<NavbarMenu>My Nav</NavbarMenu>);
-        expect(component.contains(<nav className='navbar-menu'>My Nav</nav>)).toBe(true);
-    });
-
     it('should render a div with .navbar-menu', () => {
-        const component = shallow(<NavbarMenu tag='div'>My Nav</NavbarMenu>);
+        const component = shallow(<NavbarMenu>My Nav</NavbarMenu>);
         expect(component.contains(<div className='navbar-menu'>My Nav</div>)).toBe(true);
     });
 
-     it('should render a nav with .navbar-menu and modifiers', () => {
+    it('should render a div with .navbar-menu', () => {
+        const component = shallow(<NavbarMenu tag='nav'>My Nav</NavbarMenu>);
+        expect(component.contains(<nav className='navbar-menu'>My Nav</nav>)).toBe(true);
+    });
+
+     it('should render a div with .navbar-menu and modifiers', () => {
         const component = shallow(<NavbarMenu isActive><span>Any Content</span></NavbarMenu>);
         expect(component.hasClass('navbar-menu')).toBe(true);
         expect(component.hasClass('is-active')).toBe(true);
     });
 
-     it('should render a nav with .navbar-menu, modifiers and custom classNames', () => {
+     it('should render a div with .navbar-menu, modifiers and custom classNames', () => {
         const component = shallow(<NavbarMenu isActive className='custom'><span>Any Content</span></NavbarMenu>);
         expect(component.hasClass('navbar-menu')).toBe(true);
         expect(component.hasClass('is-active')).toBe(true);
         expect(component.hasClass('custom')).toBe(true);
     });
 
-    it('should render a nav with .navbar-menu and custom classNames', () => {
+    it('should render a div with .navbar-menu and custom classNames', () => {
         const component = shallow(<NavbarMenu className='custom'><span>Any Content</span></NavbarMenu>);
         expect(component.hasClass('navbar-menu')).toBe(true);
         expect(component.hasClass('custom')).toBe(true);
