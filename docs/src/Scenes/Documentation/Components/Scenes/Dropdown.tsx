534928d801e794d759890d5f8ea987e33eb1d04a
omganunicorn <kralos@gmail.com>
2017-08-10 15:46:16 -0500

    Updates the dropdown component with alignment and active attribute support

--- docs/src/Scenes/Documentation/Components/Scenes/Dropdown.tsx
+++ docs/src/Scenes/Documentation/Components/Scenes/Dropdown.tsx
@@ -9,9 +9,14 @@ import { Column, Columns } from './../../../../../../src';
 import { Container, Subtitle, Title } from './../../../../../../src';
 import { Dropdown, DropdownContent, DropdownDivider, DropdownItem, DropdownMenu, DropdownTrigger } from './../../../../../../src';
 import { Icon } from './../../../../../../src';
-import { noModifiers, render } from './../../Overview/Scenes/Helpers';
+import { noModifiers } from './../../Overview/Scenes/Helpers';
 
 const DropdownRows: Docs.Row[] = [
+    {
+        description: <td><code>right</code></td>,
+        prop: 'isAlign',
+        type: 'string',
+    },
     {
         description: '',
         prop: 'isActive',
@@ -20,39 +25,47 @@ const DropdownRows: Docs.Row[] = [
 ];
 
 const DropdownBasic = ({ isDropdownOpen, openDropdown }) => (
-  <Dropdown isActive>
+  <Dropdown>
     <DropdownTrigger onClick={openDropdown}>
       <Button isOutlined aria-haspopup="true" aria-controls="dropdown-menu">
         <span>Dropdown button</span>
-        <Icon icon='angle-down' isSize="small" />
+        <Icon icon="angle-down" isSize="small" />
       </Button>
     </DropdownTrigger>
     <DropdownMenu className={isDropdownOpen ? 'is-block' : ''}>
       <DropdownContent>
         <DropdownItem href="#">First item</DropdownItem>
-        <DropdownItem href="#">Second item</DropdownItem>
+        <DropdownItem href="#" isActive>Second item</DropdownItem>
         <DropdownItem href="#">Third item</DropdownItem>
-        <DropdownDivider></DropdownDivider>
-        <DropdownItem href="#">Last item</DropdownItem>
+        <DropdownDivider />
+        <DropdownItem href="#">Item between dividers</DropdownItem>
+        <DropdownDivider />
+        <DropdownItem>
+            <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+        </DropdownItem>
       </DropdownContent>
     </DropdownMenu>
   </Dropdown>
 );
 
 const DropdownExample = `<Dropdown>
-  <DropdownTrigger>
+  <DropdownTrigger onClick={openDropdown}>
     <Button isOutlined aria-haspopup="true" aria-controls="dropdown-menu">
       <span>Dropdown button</span>
-      <Icon icon='angle-down' isSize="small" />
+      <Icon icon="angle-down" isSize="small" />
     </Button>
   </DropdownTrigger>
   <DropdownMenu>
     <DropdownContent>
       <DropdownItem href="#">First item</DropdownItem>
-      <DropdownItem href="#">Second item</DropdownItem>
+      <DropdownItem href="#" isActive>Second item</DropdownItem>
       <DropdownItem href="#">Third item</DropdownItem>
-      <DropdownDivider></DropdownDivider>
-      <DropdownItem href="#">Last item</DropdownItem>
+      <DropdownDivider />
+      <DropdownItem href="#">Item between dividers</DropdownItem>
+      <DropdownDivider />
+      <DropdownItem>
+          <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+      </DropdownItem>
     </DropdownContent>
   </DropdownMenu>
 </Dropdown>
@@ -60,7 +73,7 @@ const DropdownExample = `<Dropdown>
 
 class DropdownDocs extends React.Component<{}, { isDropdownOpen: boolean }> {
     state = {
-        isDropdownOpen: true
+        isDropdownOpen: true,
     };
 
     openDropdown = () => this.setState((state) => ({ ...state, isDropdownOpen: !state.isDropdownOpen }));
@@ -70,16 +83,39 @@ class DropdownDocs extends React.Component<{}, { isDropdownOpen: boolean }> {
         <Container>
             <Title>Dropdown</Title>
             <Columns>
-                <Column isSize={4}>
+                <Column isSize="1/3">
                   <DropdownBasic isDropdownOpen={this.state.isDropdownOpen} openDropdown={this.openDropdown} />
                 </Column>
-                <Column><Highlight>{DropdownExample}</Highlight></Column>
+                <Column className="is-clipped">
+                  <Highlight>{DropdownExample}</Highlight>
+                </Column>
             </Columns>
+
             <hr />
             <Title>Dropdown</Title>
             <TableDocs rows={DropdownRows} />
+
+            <hr />
+            <Title>DropdownTrigger</Title>
+            <Subtitle>{noModifiers}</Subtitle>
+
+            <hr />
+            <Title>DropdownMenu</Title>
+            <Subtitle>{noModifiers}</Subtitle>
+
+            <hr />
+            <Title>DropdownContent</Title>
+            <Subtitle>{noModifiers}</Subtitle>
+
+            <hr />
+            <Title>DropdownItemRows</Title>
+            <Subtitle>
+                By default, <strong>{'<DropdownItem />'}</strong> renders a <strong>{'<div />'}</strong> element,
+                but it will render an <strong>{'<a/>'}</strong> element if you provide <strong>href</strong> prop.
+                 <strong>E.g.</strong> <em><strong>{'<DropdownItem href="#""/>'}</strong></em>.
+            </Subtitle>
         </Container>
-      )
+      );
     }
 }
 
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,17 +1,31 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
 
-import { Bulma, withHelpersModifiers } from './../../bulma';
+import {
+  Bulma,
+  getActiveModifiers, getAlignmentModifiers,
+  removeActiveModifiers, removeAlignmentProps,
+  withHelpersModifiers,
+} from './../../bulma';
+import { combineModifiers, getHTMLProps } from './../../helpers';
 
-export interface Dropdown<T> extends Bulma.Tag, Bulma.Active, React.HTMLProps<T> {
+export interface Dropdown<T> extends Bulma.Active, Bulma.Alignment, Bulma.Tag, React.HTMLProps<T> {
   isActive?: boolean;
-  isHoverable?: boolean;
+  isAlign?: 'right';
 }
 
 export function Dropdown({ tag = 'div', ...props }: Dropdown<HTMLElement>) {
-    const className = classNames('dropdown', props.className);
+    const className = classNames(
+        'dropdown',
+        {
+            ...combineModifiers(props, getActiveModifiers, getAlignmentModifiers),
+        },
+        props.className,
+    );
 
-    return React.createElement(tag, { ...props, className });
+    const HTMLProps = getHTMLProps(props, removeActiveModifiers, removeAlignmentProps);
+
+    return React.createElement(tag, { ...HTMLProps, className });
 }
 
 const HOC = /*@__PURE__*/withHelpersModifiers(Dropdown);
