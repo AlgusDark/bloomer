297c483a70b388ba4e880dfd3bcfcdbdfbc10994
omganunicorn <kralos@gmail.com>
2018-01-03 12:07:10 -0600

    Adds test cases for the Dropdown component.

---
+++ test/components/Dropdown/Dropdown.test.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import { Dropdown } from './../../../src/components/Dropdown/Dropdown';
+
+describe('Dropdown', () => {
+    it('should render div with .dropdown that is active', () => {
+        const component = shallow(<Dropdown isActive>My Dropdown</Dropdown>);
+        expect(component.hasClass('dropdown')).toBe(true);
+        expect(component.hasClass('is-active')).toBe(true);
+    });
+
+    it('should show a div with .dropdown and modifiers', () => {
+        const component = shallow(<Dropdown isAlign="right" isHoverable>My Dropdown</Dropdown>);
+        expect(component.hasClass('dropdown')).toBe(true);
+        expect(component.hasClass('is-hoverable')).toBe(true);
+        expect(component.hasClass('is-right')).toBe(true);
+    });
+});
